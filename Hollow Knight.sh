12568d4c44cf8b75e1d03d7b80dabb93f9bd7cf9
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-17 16:48:22 -0400

    Update Hollow Knight.sh

--- Hollow Knight.sh
+++ Hollow Knight.sh
@@ -14,25 +14,34 @@ get_controls
 
 # Variables
 GAMEDIR="/$directory/windows/hollow_knight"
+EXEC="hollow_knight.exe"
 
-# CD and set permissions
+# CD and set log
 cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-$ESUDO chmod +x -R $GAMEDIR/*
 
 # Display loading splash
 [ "$CFW_NAME" == "muOS" ] && $ESUDO $GAMEDIR/splash "splash.png" 1
-$ESUDO $GAMEDIR/splash "splash.png" 80000 & 
+$ESUDO $GAMEDIR/splash "splash.png" 50000 & 
 
 # Exports
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export WINEPREFIX=~/.wine64
 export WINEDEBUG=-all
 
+# Determine architecture
+if file "$GAMEDIR/data/$EXEC" | grep -q "PE32" && ! file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEARCH=win32
+    export WINEPREFIX=~/.wine32
+elif file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEPREFIX=~/.wine64
+else
+    echo "Unknown file format"
+fi
+
 # Install dependencies
-if ! winetricks list-installed | grep -q "^dxvk$"; then
+if ! winetricks list-installed | grep -q "^dxvk2041$"; then
     pm_message "Installing dependencies."
-    winetricks dxvk
+    winetricks dxvk2041
 fi
 
 # Config Setup
@@ -40,8 +49,8 @@ mkdir -p $GAMEDIR/config
 bind_directories "$WINEPREFIX/drive_c/users/root/AppData/LocalLow/Team Cherry/Hollow Knight" "$GAMEDIR/config"
 
 # Run the game
-$GPTOKEYB "hollow_knight.exe" -c "./hollow.gptk" &
-box64 wine64 "./data/hollow_knight.exe"
+$GPTOKEYB "EXEC" -c "$GAMEDIR/hollow.gptk" &
+box64 wine "$GAMEDIR/data/$EXEC"
 
 # Kill processes
 wineserver -k
